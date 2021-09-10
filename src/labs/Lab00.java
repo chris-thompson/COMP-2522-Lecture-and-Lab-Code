3e40a3ed8a1d98d3bfda3ee890809ad7e7a78860
Chris Thompson <chris_thompson@bcit.ca>
2021-09-10 13:15:48 -0700

    Tidy and (partially-)comment code for Lab 00.

--- src/labs/Lab00.java
+++ src/labs/Lab00.java
@@ -2,25 +2,36 @@ package labs;
 
 import java.util.ArrayList;
 
-public class Lab01 {
+/**
+ * Some sample code we generated during Lab 0.
+ *
+ * @author BCIT
+ * @version 2021
+ */
+public class Lab00 {
 
+    /**
+     * Drives the program.
+     * @param args unused
+     */
     public static void main(String[] args) {
 
-//        int i = 10;
-//        while (i > 0) {
-//            System.out.println(i--);
-//        }
-//        double pi = 3.14159258;
-//        do {
-//            System.out.println("This is a positive number");
-//            pi -= 1;
-//        } while (pi > 0);
+        int boundary = 10;
+        while (boundary > 0) {
+            System.out.println(boundary--);
+        }
+
+        double pi = Math.PI;
+        do {
+            System.out.println("This is a positive number");
+            pi -= 1;
+        } while (pi > 0);
 
         for (int i = 0; i < 100; ++i) {
             System.out.println(i);
         }
 
-        ArrayList<String> names = new ArrayList<String>();
+        ArrayList<String> names = new ArrayList<>();
         names.add("Marti");
         names.add("April");
         names.add("Sally");
@@ -29,13 +40,10 @@ public class Lab01 {
             System.out.println(studentName);
         }
 
-        String name = "Susan";
+        String name = "Kaz";
         String anotherName = name.toLowerCase();
         System.out.println(name);
         System.out.println(anotherName);
         System.out.println(name.toUpperCase());
-
     }
-
-
 }
