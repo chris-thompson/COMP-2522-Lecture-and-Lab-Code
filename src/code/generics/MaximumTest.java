07901a3fa6fdaabf7e1fd2380320e80984294618
Chris <chris_thompson@bcit.ca>
2021-10-27 12:20:45 -0700

    Tidy code for generics

--- src/code/generics/MaximumTest.java
+++ src/code/generics/MaximumTest.java
@@ -6,7 +6,9 @@ package code.generics;
  * @author BCIT
  * @version 2020
  */
-public class MaximumTest {
+public final class MaximumTest {
+
+    private MaximumTest() { }
 
     /**
      * Returns the maximum of the three elements.
@@ -16,23 +18,25 @@ public class MaximumTest {
      * @param <T> the data type
      * @return the 'maximum' of x, y, and z
      */
-    public static <T extends Comparable<T> > T maximum(T x, T y, T z) {
+    public static <T extends Comparable<T>> T maximum(final T x, final T y, final T z) {
         T max = x; // assume x is initially the largest
 
-        if (y.compareTo(max) > 0)
+        if (y.compareTo(max) > 0) {
             max = y; // y is the largest so far
+        }
 
-        if (z.compareTo(max) > 0)
+        if (z.compareTo(max) > 0) {
             max = z; // z is the largest
+        }
 
         return max; // returns the largest object
     }
 
     /**
      * Drives the program.
-     * @param args
+     * @param args unused
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         System.out.printf("Maximum of %d, %d and %d is %d%n%n",
                 3, 4, 5, maximum(3, 4, 5));
         //maximum(3, "hello", 7);
